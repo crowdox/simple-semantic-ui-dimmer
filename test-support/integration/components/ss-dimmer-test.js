f6d275c40494e0a3b395e001240fca98a443f42c
Jeff Seifert <jeffse@gmail.com>
2020-03-31 23:50:34 -0400

    Convert moduleForComponent to setupRenderingTest invocation Also replacing jQuery references with assert.dom() calls.

--- test-support/integration/components/ss-dimmer-test.js
+++ test-support/integration/components/ss-dimmer-test.js
@@ -1,54 +1,51 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ss-dimmer', 'Integration | Component | ss dimmer', {
-  integration: true
-});
+module('Integration | Component | ss dimmer', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders', function(assert) {
-  assert.expect(2);
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  test('it renders', async function(assert) {
+    assert.expect(2);
+    // Set any properties with this.set('myProperty', 'value');
+    // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{ss-dimmer}}`);
+    await render(hbs`{{ss-dimmer}}`);
 
-  assert.equal(this.$().text().trim(), '');
+    assert.dom().hasText('');
 
-  // Template block usage:
-  this.render(hbs`
-    {{#ss-dimmer}}
-      template block text
-    {{/ss-dimmer}}
-  `);
+    // Template block usage:
+    await render(hbs`
+      {{#ss-dimmer}}
+        template block text
+      {{/ss-dimmer}}
+    `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
-});
+    assert.dom().hasText('template block text');
+  });
 
-test('dimmer shows and hides on click', function(assert) {
-  assert.expect(6);
-  this.set('isActive', false);
-  // Template block usage:
-  this.render(hbs`
-    <div class="ui segment">
-      {{ss-dimmer isActive=isActive transitionDuration=0}}
-    </div>
-  `);
-
-  assert.equal(this.$('.ui.segment .ui.dimmer').length, 1, "UI Dimmer not found");
-  assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 0, "An active UI Dimmer was found and shouldn't be");
-  this.set('isActive', true);
-
-  let done = assert.async();
-  setTimeout(() => {
-    assert.equal(this.$('.ui.segment .ui.dimmer').length, 1, "UI Dimmer not found");
-    assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 1, "No active UI Dimmer was found"); //broken
+  test('dimmer shows and hides on click', async function(assert) {
+    assert.expect(6);
     this.set('isActive', false);
+    // Template block usage:
+    await render(hbs`
+      <div class="ui segment">
+        {{ss-dimmer isActive=isActive transitionDuration=0}}
+      </div>
+    `);
 
-    setTimeout(() => {
-      assert.equal(this.$('.ui.segment .ui.dimmer').length, 1, "UI Dimmer not found");
-      assert.equal(this.$('.ui.segment .ui.dimmer.active').length, 0, "An active UI Dimmer was found and shouldn't be");
-      done();
-    }, 100);
-  }, 100);
+    assert.dom('.ui.segment .ui.dimmer').exists();
+    assert.dom('.ui.segment .ui.dimmer.active').doesNotExist();
+
+    this.set('isActive', true);
+    await settled();
+    assert.dom('.ui.segment .ui.dimmer').exists();
+    assert.dom('.ui.segment .ui.dimmer.active').exists();
+
+    this.set('isActive', false);
+    await settled();
+    assert.dom('.ui.segment .ui.dimmer').exists();
+    assert.dom('.ui.segment .ui.dimmer.active').doesNotExist();
+  });
 });
-
